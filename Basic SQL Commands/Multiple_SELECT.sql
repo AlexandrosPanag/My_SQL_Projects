3277373c28b22dfe383f79c338a70071435de01d
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-09-12 16:53:05 +0300

    Add files via upload

---
+++ Basic SQL Commands/Multiple_SELECT.sql
@@ -0,0 +1,4 @@
+SELECT NAME , SURNAME ,BIRTHDATE
+FROM DATABASE_MYCOMPANY;
+
+/* With the select command you you can view specific columns that you want in this example we need name , surname , birthdate to be viewed */
