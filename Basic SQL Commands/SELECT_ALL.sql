8d27488b7bc90af2414801e5ce8b0c571bab2cc7
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-09-12 16:59:22 +0300

    Add files via upload

---
+++ Basic SQL Commands/SELECT_ALL.sql
@@ -0,0 +1,5 @@
+SELECT *
+FROM DATABASE_MYCOMPANY;
+
+/* To retrieve everything and show all data to the table that we have created we use an asterisk */
+/* to the SELECT command  (*) the following command will show everything from the DATABASE_MYCOMPANY table */
