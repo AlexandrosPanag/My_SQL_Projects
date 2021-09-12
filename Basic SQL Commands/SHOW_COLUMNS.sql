1d1ef52032e6e0d6a04746070f4a523cc3744fef
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-09-12 16:48:39 +0300

    Add files via upload

---
+++ Basic SQL Commands/SHOW_COLUMNS.sql
@@ -0,0 +1,3 @@
+SHOW COLUMNS FROM NAME
+
+/* The SHOW COLLUMNS displays information about the columns in a given table for example it will show all the columns in the "NAME" category (list all the names) */
