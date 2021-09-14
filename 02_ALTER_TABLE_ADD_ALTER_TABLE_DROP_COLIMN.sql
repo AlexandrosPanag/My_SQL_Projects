30ae32d840f88f8965a1b15a220781a0b4b30ef8
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-09-14 09:12:21 +0300

    Add files via upload

---
+++ 02_ALTER_TABLE_ADD_ALTER_TABLE_DROP_COLIMN.sql
@@ -0,0 +1,7 @@
+ALTER TABLE DATABASE_MYCOMPANY ADD FATHER'S_NAME VARCHAR(30);
+
+/* This command will alter the table by adding the "Father's name" column */
+
+ALTER TABLE DATABASE_MYCOMPANY DROP COLUMN FATHER'S_NAME'
+
+/* This command will drop the father's name column entirely */
