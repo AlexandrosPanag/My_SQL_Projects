9f3373059209c5408b26c98ebab3078658454c41
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-09-14 09:20:53 +0300

    Add files via upload

---
+++ 03_ALTER_TABLE_MODIFY.sql
@@ -0,0 +1,5 @@
+ALTER TABLE DATABASE_MYCOMPANY MODIFY PHONE VARCHAR(30);
+
+
+/* With this command we change the phone data type from int (see:01_Creating a database.sql) */
+/* to varchar(30) type */ 
