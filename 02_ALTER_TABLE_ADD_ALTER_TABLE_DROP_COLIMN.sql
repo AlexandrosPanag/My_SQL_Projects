2204cf662b20f16544ec40ca27491a97e5e0cc10
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-09-14 09:12:54 +0300

    Update 02_ALTER_TABLE_ADD_ALTER_TABLE_DROP_COLIMN.sql

--- 02_ALTER_TABLE_ADD_ALTER_TABLE_DROP_COLIMN.sql
+++ 02_ALTER_TABLE_ADD_ALTER_TABLE_DROP_COLIMN.sql
@@ -2,6 +2,10 @@ ALTER TABLE DATABASE_MYCOMPANY ADD FATHER'S_NAME VARCHAR(30);
 
 /* This command will alter the table by adding the "Father's name" column */
 
-ALTER TABLE DATABASE_MYCOMPANY DROP COLUMN FATHER'S_NAME'
+
+
+
+
+ALTER TABLE DATABASE_MYCOMPANY DROP COLUMN FATHER'S_NAME;
 
 /* This command will drop the father's name column entirely */
