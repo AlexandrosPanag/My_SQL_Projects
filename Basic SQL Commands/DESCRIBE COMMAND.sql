0d223d861f6d9ea67b17fe90e5cf285ebf940a78
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-09-15 08:42:15 +0300

    Add files via upload

---
+++ Basic SQL Commands/DESCRIBE COMMAND.sql
@@ -0,0 +1,4 @@
+DESCRIBE DATABASE_MYCOMPANY
+
+/* The output will show the various columns the table is made of, the data type of each column, and other related metadata. */
+/* SOURCE : www.educative.io/courses/introductory-guide-to-sql */
