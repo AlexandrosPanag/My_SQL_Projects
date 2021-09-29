97faf294cacadb9ec4d0ecd07a02877c72ae7dcb
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-09-29 13:53:25 +0300

    Add files via upload

---
+++ 11_Deleting Data.sql
@@ -0,0 +1,12 @@
+DELETE FROM DATABASE_MYCOMPANY WHERE NAME="John";
+
+/* It will delete all of john doe's row */
+
+DELETE FROM DATABASE_MYCOMPANY WHERE IDNUMBER="JO1111111";
+
+/*this command will also delete all of john doe's data */
+
+
+DELETE FROM DATABASE_MYCOMPANY;
+
+/* This command will delete everything from database_mycompany */
